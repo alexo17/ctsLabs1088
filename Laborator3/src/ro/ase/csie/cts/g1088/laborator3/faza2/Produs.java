eae89d3c669f1ef45dd85ddbdcf98b7002601287
alexo17 <opreacatalin18@stud.ase.ro>
2021-03-11 13:30:21 +0200

    laborator 3 - done

--- Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza2/Produs.java
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza2/Produs.java
@@ -1,4 +1,4 @@
-package ro.ase.csie.cts.g1088.laborator3.faza1;
+package ro.ase.csie.cts.g1088.laborator3.faza2;
 
 import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptiePretInvalid;
 import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptieVechimeClient;
@@ -7,36 +7,52 @@ public class Produs {
 
 	public static final int VECHIME_CLIENT_MAXIMA = 10;
 	public static final float DISCOUNT_CLIENT_MAXIM = 0.15f;
-
-	public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni) throws ExceptiePretInvalid, ExceptieVechimeClient {
+	
+	public static void validarePret(float pretInitial) throws ExceptiePretInvalid {
 		if (pretInitial <= 0) {
 			throw new ExceptiePretInvalid();
 		}
-		
+	}
+	
+	public static void validareVechimeClient(int vechimeClientInAni) throws ExceptieVechimeClient {
 		if (vechimeClientInAni < 0) {
 			throw new ExceptieVechimeClient();
 		}
-		
-		float pretFinal = 0;
-		float discountFidelitate = (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM
+	}
+	
+	public static float getDiscountFidelitate(int vechimeClientInAni) {
+		return (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM
 				: (float) vechimeClientInAni / 100;
-		
+	}
+	
+	public static float getPretCuDiscount(float pretInitial, float discount) {
+		return pretInitial - (discount * pretInitial);
+	}
+	
+	
+
+	public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni) throws ExceptiePretInvalid, ExceptieVechimeClient {
+		validarePret(pretInitial);
+		validareVechimeClient(vechimeClientInAni);
+		float pretFinal = 0;
+		float discountFidelitate = getDiscountFidelitate(vechimeClientInAni);
 		float discount = tipProdus.getDiscount();
+		float valoareDiscountTipProdus = getPretCuDiscount(pretInitial, discount);
 		switch(tipProdus) {
 			case NOU:
 				pretFinal = pretInitial;
 				break;
 			case DISCOUNT:
-				pretFinal = (pretInitial - (discount * pretInitial))
-				- discountFidelitate * (pretInitial - (discount * pretInitial));
+				pretFinal = valoareDiscountTipProdus
+				- discountFidelitate * valoareDiscountTipProdus;
 				break;
 			case STOC_LIMITAT:
-				pretFinal = (pretInitial - (discount * pretInitial))
-				- discountFidelitate * (pretInitial - (discount * pretInitial));
+				pretFinal = valoareDiscountTipProdus
+				- discountFidelitate * valoareDiscountTipProdus;
 				break;
 			case VECHI:
-				pretFinal = (pretInitial - (discount * pretInitial))
-				- discountFidelitate * (pretInitial - (discount * pretInitial));
+				pretFinal = valoareDiscountTipProdus
+				- discountFidelitate * valoareDiscountTipProdus;
 				break;
 			default:
 				throw new UnsupportedOperationException("Un simbol din enumerare nu este procesat");
