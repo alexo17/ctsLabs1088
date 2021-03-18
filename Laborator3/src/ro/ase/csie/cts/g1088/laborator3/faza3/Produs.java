707573eb96133faeed38027fe299db5596eb6899
alexo17 <opreacatalin18@stud.ase.ro>
2021-03-18 13:24:17 +0200

    Lab 4 - Done

--- Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza3/Produs.java
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza3/Produs.java
@@ -4,11 +4,32 @@ import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptiePretInvalid;
 import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptieVechimeClient;
 import ro.ase.csie.cts.g1088.laborator3.faza3.servicii.InterfataMarketing;
 import ro.ase.csie.cts.g1088.laborator3.faza3.servicii.InterfataValidare;
+import ro.ase.csie.cts.g1088.laborator3.faza3.servicii.ServiciuValidari;
+import ro.ase.csie.cts.g1088.laborator3.faza3.servicii.StrategieMarketing2021;
 
 public class Produs {
 	
 	private InterfataMarketing serviciuMk = null;
 	private InterfataValidare serviciuValidare = null;
+	
+	public Produs(InterfataMarketing mk, InterfataValidare validare) {
+		this.setStrategieMarketing(mk);
+		this.setServiciuValidari(validare);
+	}
+	
+	public void setStrategieMarketing(InterfataMarketing mk) {
+		if (mk == null) {
+			throw new NullPointerException();
+		}
+		this.serviciuMk = mk;
+	}
+	
+	public void setServiciuValidari(InterfataValidare validare) {
+		if (validare == null) {
+			throw new NullPointerException();
+		}
+		this.serviciuValidare = validare;
+	}
 	
 	public static float getPretCuDiscount(float pretInitial, float discount) {
 		return pretInitial - (discount * pretInitial);
