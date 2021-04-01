d27c6d3438b066ebbc374a1caba4ab22c57ac48d
alexo17 <opreacatalin18@stud.ase.ro>
2021-04-01 13:27:21 +0300

    Lab 6 - done.

---
+++ Lab 6 - Builder and Factory/src/ro/ase/csie/cts/g1088/dp/factory/CaracterMarvel.java
@@ -0,0 +1,26 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+public class CaracterMarvel extends CaracterAbstract {
+
+	int putere;
+	
+	
+	
+	public CaracterMarvel(int putere, String nume) {
+		this.nume = nume;
+		this.putere = putere;
+	}
+
+	@Override
+	public void alearga() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sare() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ Lab 6 - Builder and Factory/src/ro/ase/csie/cts/g1088/dp/factory/TestFactory.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+import java.util.ArrayList;
+
+public class TestFactory {
+
+	public static void main(String[] args) {
+//		CaracterAbstract mickeyMouse = new CaracterDisney("rosu", "Mickey Mouse");
+//		CaracterAbstract spiderMan = new CaracterMarvel(100, "Spiderman");
+		
+		CaracterAbstract mickeyMouse = FactoryCaractere.getCaracter(TipCaracter.DISNEY, "Mickey Mouse");
+		
+		ArrayList<CaracterAbstract> caractere = new ArrayList<>();
+	}
+
+}
