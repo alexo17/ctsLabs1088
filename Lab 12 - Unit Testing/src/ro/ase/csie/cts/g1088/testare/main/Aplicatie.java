2512cfcfd13a44126e52185993c97dd21b7a55cc
alexo17 <opreacatalin18@stud.ase.ro>
2021-05-20 13:29:21 +0300

    Lab 12 - done

---
+++ Lab 12 - Unit Testing/src/ro/ase/csie/cts/g1088/testare/main/Aplicatie.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.g1088.testare.main;
+
+import ro.ase.csie.cts.g1088.testare.modele.OperatiiMatematice;
+
+public class Aplicatie {
+
+	public static void main(String[] args) {
+		System.out.println("Aplicatie ruleaza");
+		
+//		int a = 5, b = 6, sumaAsteptata = 11;
+//		int sumaCalculata = OperatiiMatematice.suma(a, b);
+//		
+//		if (sumaAsteptata == sumaCalculata) {
+//			System.out.println("Suma este corecta");
+//		} else {
+//			System.out.println("Ne trebuie un curs de mate");
+//		}
+	}
+
+}
